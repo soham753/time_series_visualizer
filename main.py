b81dae44137e748a7fa53b1f93fbaae2e4064058
soham753 <159909074+soham753@users.noreply.github.com>
2025-04-26 12:43:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+
+# Run and save plots
+fig1 = draw_line_plot()
+fig1.savefig('line_plot.png')
+
+fig2 = draw_bar_plot()
+fig2.savefig('bar_plot.png')
+
+fig3 = draw_box_plot()
+fig3.savefig('box_plot.png')
